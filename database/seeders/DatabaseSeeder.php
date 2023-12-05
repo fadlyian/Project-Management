98f2cda1eba2e0fa18a0b996b24b073e77331934
fadlyian <mansyuraw24@gmail.com>
2023-12-05 21:50:47 +0700

    install breeze/reactJS | add docker compose | add seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('users')->insert([
+            'name' => 'super-admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
