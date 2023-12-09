a85a0509510c7e0c764618ab292384cb156628fc
fadlyian <mansyuraw24@gmail.com>
2023-12-09 15:41:25 +0700

    [created] Project of Model, Controller, Migration, and Seeder

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $nameProject = [
+            'STKI',
+            'Kriptografi',
+        ];
+
+        foreach($nameProject as $name){
+            DB::table('projects')->insert([
+                'nameProject' => $name,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
