3a36702ae472c2e2d82f698bd6e301d90bde702a
fadlyian <mansyuraw24@gmail.com>
2023-12-09 16:34:34 +0700

    [created] Project User(pivot table) of Model, Controller, Migration, Seeder

---
+++ app/Models/ProjectUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectUser extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'project_user';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'project_id',
+        'role_id',
+    ];
+}
