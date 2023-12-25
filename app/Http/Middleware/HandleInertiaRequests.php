08f4e3c92df1664e2e3425cee9853836314a3b05
fadlyian <mansyuraw24@gmail.com>
2023-12-25 16:10:20 +0700

    ""

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,6 @@ class HandleInertiaRequests extends Middleware
             ...parent::share($request),
             'auth' => [
                 'user' => $request->user(),
-                'projects' => $user?->projects()->get(),
             ],
             'ziggy' => fn () => [
                 ...(new Ziggy)->toArray(),
